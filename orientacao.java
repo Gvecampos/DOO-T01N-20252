745cc85d0054b2b04e17672eabaff3a97ad576b1
Gvecampos <gvecampos3@gmail.com>
2025-07-31 22:42:12 -0300

    1 commit

---
+++ orientacao.java
@@ -0,0 +1,25 @@
+
+
+import java.util.*;           // Collections, ArrayList, HashMap, etc.
+import java.io.*;            // File, BufferedReader, etc.
+import java.time.*;          // LocalDate, LocalDateTime, etc.
+import java.util.concurrent.*; // ExecutorService, Future, etc.
+import java.lang.reflect.*;  // Class, Method, Field, etc.
+import java.net.*;           // URL, Socket, etc.
+import java.util.regex.*;    // Pattern, Matcher
+import java.util.logging.*;  // Logger, Level
+
+public class orientacao {
+    
+    public static void main(String[] args) {
+
+        System.out.println("gabriel  hello world"); 
+    }
+}
+
+
+
+
+
+
+
